da61b2d2aa6fee9a30ccd0eb5025108b76848428
Clement Hoang <clement.hoang24@gmail.com>
2013-05-24 16:02:44 -0300

    Create goaltender.java

---
+++ src/goaltender.java
@@ -0,0 +1,10 @@
+
+public class goaltender extends Coach{
+
+  public goaltender(String fname, String lname, boolean gender,
+			double height, double weight, int month, int day, int year,
+			int place, int status) {
+		super(fname, lname, gender, height, weight, month, day, year, place, status);
+	}
+
+}
