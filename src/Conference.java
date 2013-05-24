3c2adbbfb4045f2e124b6b8b67bfe75b40279e51
AVkid019 <AVkid019@gmail.com>
2013-05-23 22:23:45 -0300

    Update Conference.java

--- src/Conference.java
+++ src/Conference.java
@@ -1,5 +1,44 @@
-
 public class Conference {
-  private String name; //Name of conference (Documenting required)
+	private String confName;
 	private Division [] divisions; //A group of divisions
+	
+	//Accessor Methods
+		/**
+		 * Returns the conference name 
+		 * @return divName
+		 */
+		public String getconfName() {
+			return confName;
+		}
+
+		/**
+		 * Returns the division name
+		 * @return conference
+		 */
+		public Division [] getdivisions() {
+			return divisions;
+		}
+
+	//Mutator Methods
+		/**
+		 * Stores parameter to conference
+		 * @param x of type Integer
+		 */
+
+		public void putconfName(int x) {
+			if (x>=4 && x<=6)
+				confName = "Western";
+			else if (x>=1)
+				confName = "Eastern";
+		}
+		
+		/**
+		 * Stores parameter to divisions[]
+		 * @param x of type Division
+		 */
+		
+		public void putdivisions(Division[] x) {
+			divisions = x;
+		}
+	
 }
