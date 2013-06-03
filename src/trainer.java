998929995cf10f3cf7b00878ae0370570289acdd
AVkid019 <AVkid019@gmail.com>
2013-06-03 02:52:26 -0300

    Update trainer.java

--- src/trainer.java
+++ src/trainer.java
@@ -5,16 +5,29 @@ import java.io.PrintWriter;
 
 public class trainer extends Coach {
 	
+	/**
+   	* Constructs an object of type trainer
+	* @param  fname, lname of type string and gender of type boolean and height, weight of type double and month, day, year, place, mStatus of type int
+	* @throws IOException
+	*/
 	public trainer(String fname, String lname, boolean gender,
 			double height, double weight, int month, int day, int year,
 			int place, int mStatus) {
 		super(fname, lname, gender, height, weight, month, day, year, place, mStatus);
 	}
 	
+	/**
+   	* Constructs an object of type trainer by calling the load method
+	* @throws IOException
+	*/
 	public trainer(BufferedReader br) throws IOException {
 		load(br);
 	}
 	
+	/**
+   	* Constructs an object of type trainer by prompting for each field
+	* @throws IOException
+	*/
 	public trainer() {
 		prompt();
 	}
