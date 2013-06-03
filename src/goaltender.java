c0773ef4df34d75f35a90d3056a81af86a6c0cd0
AVkid019 <AVkid019@gmail.com>
2013-06-03 02:48:21 -0300

    Update goaltender.java

--- src/goaltender.java
+++ src/goaltender.java
@@ -5,16 +5,29 @@ import java.io.PrintWriter;
 
 public class goaltender extends Coach {
 
+	/**
+  	* Constructs an object of type goaltender
+	* @param  fname, lname of type string and gender of type boolean and height, weight of type double and month, day, year, place, mStatus of type int
+	* @throws IOException
+	*/
 	public goaltender(String fname, String lname, boolean gender,
 			double height, double weight, int month, int day, int year,
 			int place, int mStatus) {
 		super(fname, lname, gender, height, weight, month, day, year, place, mStatus);
 	}
 	
+	/**
+   	* Constructs an object of type goaltender by calling the load method
+	* @throws IOException
+	*/
 	public goaltender(BufferedReader br) throws IOException {
 		load(br);
 	}
 	
+	/**
+   	* Constructs an object of type goaltender by prompting for each field
+	* @throws IOException
+	*/
 	public goaltender() {
 		prompt();
 	}
