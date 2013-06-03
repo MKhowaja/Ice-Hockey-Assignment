d334c3981b59fb5db0b43bd05a3727de4508e193
AVkid019 <AVkid019@gmail.com>
2013-06-02 22:50:16 -0300

    Update Coach.java

--- src/Coach.java
+++ src/Coach.java
@@ -5,16 +5,29 @@ import java.io.PrintWriter;
 
 abstract class Coach extends Person { 
 	
+	/**
+	 * Constructs an object of type coach
+	 * @param fname, lname of type string and gender of type boolean and height, weight of type double and month, day, year, place, mStatus of type int
+	 * @throws IOException
+	 */
 	public Coach(String fname, String lname, boolean gender,
 			double height, double weight, int month, int day, int year,
 			int place, int mStatus) {
 		super(fname, lname, gender, height, weight, month, day, year, place, mStatus);
 	}
 	
+	/**
+	 * Runs load method in order to construct an object of type Coach
+	 * @throws IOException
+	 */
 	public Coach(BufferedReader br) throws IOException {
 		load(br);
 	}
 	
+	/**
+	 * Runs prompt method in order to prompt for all fields in Coach and then create an object of type Coach
+	 * @throws IOException
+	 */
 	public Coach() {
 		prompt();
 	}
